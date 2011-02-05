aafaa6d26f466a1ae79c212848dd2dc7cf74119b
mdlima <mdlima.giyhub@mailnull.com>
2011-02-05 11:53:07 -0200

    Finished first cut of the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,15 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,  :presence => true,
+                    :length   => { :maximum => 50 },
+                    :uniqueness => true
+
+  validates :email, :presence => true,
+                    :format   => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110204165835) do
+ActiveRecord::Schema.define(:version => 20110205005401) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
