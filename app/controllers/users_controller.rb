302b60fe086cafbc9e688c843bd604ada7182367
mdlima <mdlima.giyhub@mailnull.com>
2011-02-06 13:18:42 -0200

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,19 @@ class UsersController < ApplicationController
   end
 
   def new
-    @title = "Sign Up"
+    @user = User.new
+    @title = "Sign up"
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to the Sample App!"
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -24,7 +24,7 @@ describe "LayoutLinks" do
 
   it "should have a signup page at '/signup'" do
     get '/signup'
-    response.should have_selector('title', :content => "Sign Up")
+    response.should have_selector('title', :content => "Sign up")
   end
 
   it "should have the right links on the layout" do
@@ -38,7 +38,7 @@ describe "LayoutLinks" do
     click_link "Home"
     response.should have_selector('title', :content => "Home")
     click_link "Sign up now!"
-    response.should have_selector('title', :content => "Sign Up")
+    response.should have_selector('title', :content => "Sign up")
   end
 
 end
