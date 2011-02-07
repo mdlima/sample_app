20cbb16c4d4f5d3a947d467be19a82fa118d0205
mdlima <mdlima.giyhub@mailnull.com>
2011-02-07 00:36:42 -0200

    Done with sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to @user
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       @title = "Sign up"
       @user.password = ""
