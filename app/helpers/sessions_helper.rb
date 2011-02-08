d3b07a35fcdf5762dbe7221d642fb5d05d93c65e
mdlima <mdlima.giyhub@mailnull.com>
2011-02-08 01:28:48 -0200

    Chapter 10.2.3.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,6 +24,13 @@ module SessionsHelper
     self.current_user = nil
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
+  def deny_access
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
 
   private
 
