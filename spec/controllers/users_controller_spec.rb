8641e1c809eaf2f4c717819941bd88f980877e29
mdlima <mdlima.giyhub@mailnull.com>
2011-02-09 01:19:02 -0200

    Chapter 10.3.3

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,42 +5,57 @@ describe UsersController do
 	
 	describe "GET 'index'" do
 
-    describe "for non-signed-in users" do
-      it "should deny access" do
-        get :index
-        response.should redirect_to(signin_path)
-        flash[:notice].should =~ /sign in/i
-      end
-    end
+		describe "for non-signed-in users" do
+			it "should deny access" do
+				get :index
+				response.should redirect_to(signin_path)
+				flash[:notice].should =~ /sign in/i
+			end
+		end
 
-    describe "for signed-in users" do
+		describe "for signed-in users" do
 
-      before(:each) do
-        @user = test_sign_in(Factory(:user))
-        second = Factory(:user, :email => "another@example.com")
-        third  = Factory(:user, :email => "another@example.net")
+			before(:each) do
+				@user = test_sign_in(Factory(:user))
+				second = Factory(:user, :email => "another@example.com")
+				third	 = Factory(:user, :email => "another@example.net")
 
-        @users = [@user, second, third]
-      end
+					@users = [@user, second, third]
+					30.times do
+						@users << Factory(:user, :email => Factory.next(:email))
+					end
+				end
+			end
 
-      it "should be successful" do
-        get :index
-        response.should be_success
-      end
+			it "should be successful" do
+				get :index
+				response.should be_success
+			end
 
-      it "should have the right title" do
-        get :index
-        response.should have_selector("title", :content => "All users")
-      end
+			it "should have the right title" do
+				get :index
+				response.should have_selector("title", :content => "All users")
+			end
 
-      it "should have an element for each user" do
-        get :index
-        @users.each do |user|
-          response.should have_selector("li", :content => user.name)
-        end
-      end
-    end
-  end
+			it "should have an element for each user" do
+				get :index
+				@users[0..2].each do |user|
+					response.should have_selector("li", :content => user.name)
+				end
+			end
+
+			it "should paginate users" do
+				get :index
+				response.should have_selector("div.pagination")
+				response.should have_selector("span.disabled", :content => "Previous")
+				response.should have_selector("a", :href => "/users?page=2",
+																					 :content => "2")
+				response.should have_selector("a", :href => "/users?page=2",
+																					 :content => "Next")
+			end
+
+		end
+	end
 
 	describe "GET 'show'" do
 
@@ -192,96 +207,96 @@ describe UsersController do
 		 end
 	 end
 
-   describe "PUT 'update'" do
+	 describe "PUT 'update'" do
 
-     before(:each) do
-       @user = Factory(:user)
-       test_sign_in(@user)
-     end
+		 before(:each) do
+			 @user = Factory(:user)
+			 test_sign_in(@user)
+		 end
 
-     describe "failure" do
+		 describe "failure" do
 
-       before(:each) do
-         @attr = { :email => "", :name => "", :password => "",
-                   :password_confirmation => "" }
-       end
+			 before(:each) do
+				 @attr = { :email => "", :name => "", :password => "",
+									 :password_confirmation => "" }
+			 end
 
-       it "should render the 'edit' page" do
-         put :update, :id => @user, :user => @attr
-         response.should render_template('edit')
-       end
+			 it "should render the 'edit' page" do
+				 put :update, :id => @user, :user => @attr
+				 response.should render_template('edit')
+			 end
 
-       it "should have the right title" do
-         put :update, :id => @user, :user => @attr
-         response.should have_selector("title", :content => "Edit user")
-       end
-     end
+			 it "should have the right title" do
+				 put :update, :id => @user, :user => @attr
+				 response.should have_selector("title", :content => "Edit user")
+			 end
+		 end
 
-     describe "success" do
+		 describe "success" do
 
-       before(:each) do
-         @attr = { :name => "New Name", :email => "user@example.org",
-                   :password => "barbaz", :password_confirmation => "barbaz" }
-       end
+			 before(:each) do
+				 @attr = { :name => "New Name", :email => "user@example.org",
+									 :password => "barbaz", :password_confirmation => "barbaz" }
+			 end
 
-       it "should change the user's attributes" do
-         put :update, :id => @user, :user => @attr
-         @user.reload
-         @user.name.should  == @attr[:name]
-         @user.email.should == @attr[:email]
-       end
+			 it "should change the user's attributes" do
+				 put :update, :id => @user, :user => @attr
+				 @user.reload
+				 @user.name.should	== @attr[:name]
+				 @user.email.should == @attr[:email]
+			 end
 
-       it "should redirect to the user show page" do
-         put :update, :id => @user, :user => @attr
-         response.should redirect_to(user_path(@user))
-       end
+			 it "should redirect to the user show page" do
+				 put :update, :id => @user, :user => @attr
+				 response.should redirect_to(user_path(@user))
+			 end
 
-       it "should have a flash message" do
-         put :update, :id => @user, :user => @attr
-         flash[:success].should =~ /updated/
-       end
-     end
-   end
+			 it "should have a flash message" do
+				 put :update, :id => @user, :user => @attr
+				 flash[:success].should =~ /updated/
+			 end
+		 end
+	 end
 
 
-   describe "authentication of edit/update pages" do
+	 describe "authentication of edit/update pages" do
 
-     before(:each) do
-       @user = Factory(:user)
-     end
+		 before(:each) do
+			 @user = Factory(:user)
+		 end
 
-     describe "for non-signed-in users" do
+		 describe "for non-signed-in users" do
 
-       it "should deny access to 'edit'" do
-         get :edit, :id => @user
-         response.should redirect_to(signin_path)
-       end
+			 it "should deny access to 'edit'" do
+				 get :edit, :id => @user
+				 response.should redirect_to(signin_path)
+			 end
 
-       it "should deny access to 'update'" do
-         put :update, :id => @user, :user => {}
-         response.should redirect_to(signin_path)
-       end
-     end
-     
-     describe "for signed-in users" do
+			 it "should deny access to 'update'" do
+				 put :update, :id => @user, :user => {}
+				 response.should redirect_to(signin_path)
+			 end
+		 end
+		 
+		 describe "for signed-in users" do
 
-       before(:each) do
-         wrong_user = Factory(:user, :name => "Wrong User", :email => "user@example.net")
-         test_sign_in(wrong_user)
-       end
+			 before(:each) do
+				 wrong_user = Factory(:user, :name => "Wrong User", :email => "user@example.net")
+				 test_sign_in(wrong_user)
+			 end
 
-       it "should require matching users for 'edit'" do
-         get :edit, :id => @user
-         response.should redirect_to(root_path)
-       end
+			 it "should require matching users for 'edit'" do
+				 get :edit, :id => @user
+				 response.should redirect_to(root_path)
+			 end
 
-       it "should require matching users for 'update'" do
-         put :update, :id => @user, :user => {}
-         response.should redirect_to(root_path)
-       end
-     end
-     
-   end
+			 it "should require matching users for 'update'" do
+				 put :update, :id => @user, :user => {}
+				 response.should redirect_to(root_path)
+			 end
+		 end
+		 
+	 end
 
 
 end
