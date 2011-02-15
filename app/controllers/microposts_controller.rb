4e40dcbb254872d029ec10b1a3f19515fdb00e1f
mdlima <mdlima.giyhub@mailnull.com>
2011-02-15 10:53:57 -0300

    Chapter 11.3.4

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   before_filter :authenticate, :only => [:create, :destroy]
-
+  before_filter :authorized_user, :only => :destroy
+  
   def create
     @micropost  = current_user.microposts.build(params[:micropost])
     if @micropost.save
@@ -13,6 +14,15 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_back_or root_path
   end
+
+  private
+
+    def authorized_user
+      @micropost = Micropost.find(params[:id])
+      redirect_to root_path unless current_user?(@micropost.user)
+    end
   
 end
