47e94721d816bf43c8e3d2bf1220c4907dae4033
mdlima <mdlima.giyhub@mailnull.com>
2011-03-27 02:14:53 -0300

    Working on production environment config.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,8 @@
+if ENV['RAILS_ENV'] == "production"
+	ENV['GEM_PATH'] = '/home/boscolotshirt/.gems'
+	Gem.clear_paths
+end
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
